caa1299ba55e9829f5cf80acd6b8a2bba06912e4
Mirko Westermeier <mirko@westermeier.de>
2012-02-06 12:31:06 +0100

    ...

--- webapp.pl
+++ webapp.pl
@@ -1,35 +1,25 @@
 #!/usr/bin/env perl
 use Mojo::Base -strict;
 
+# use local lib (if Contenticious isn't installed)
+use FindBin '$Bin'; use lib "$Bin/../lib";
+use Contenticious;
+use Contenticious::Commands;
 use Mojolicious::Commands;
 use File::Basename 'dirname';
-use File::Spec;
-
-# use local lib (if Contenticious isn't installed)
-use lib join '/', File::Spec->splitdir(dirname(__FILE__)), 'lib';
-use Contenticious::Commands;
-
-# make sure Contenticious is installed
-eval { use Contenticious };
-die "Whoops: Contenticious not accessible!\n" if $@;
 
 # use Contenticious
 $ENV{MOJO_HOME} = dirname(__FILE__);
 my $app = Contenticious->new;
 
-# contenticious commands
-if (defined $ARGV[0] and $ARGV[0] ~~ [qw( dump init )]) {
-
-    # init
-    my $commands = Contenticious::Commands->new(app => $app);
-
-    # execute
-    my $command = shift @ARGV;
-    $commands->$command;
+# Contenticious dump command
+if (defined $ARGV[0] and $command eq 'dump') {
+    Contenticious::Commands->new(app => $app)->dump;
 }
 
-# Mojolicious command system
+# use Contenticious as mojo app
 else {
-    $ENV{MOJO_APP} = $app;
+    $ENV{MOJO_HOME} = dirname(__FILE__);
+    $ENV{MOJO_APP}  = $app;
     Mojolicious::Commands->start;
 }
