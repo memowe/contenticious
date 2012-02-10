47ae656e2812634c29bf7b5dfbf2903acf7802be
Mirko Westermeier <mirko@westermeier.de>
2012-02-10 18:02:55 +0100

    API-POD for Contenticious::Content

--- lib/Contenticious/Content.pm
+++ lib/Contenticious/Content.pm
@@ -58,4 +58,54 @@ sub empty_cache {
 }
 
 1;
+
 __END__
+
+=head1 NAME
+
+Contenticious::Content - content for Contenticious
+
+=head1 SYNOPSIS
+
+    use Contenticious::Content;
+    my $content = Contenticious::Content->new(pages_dir => 'pages');
+    my $node    = $content->find('foo/bar');
+
+=head1 DESCRIPTION
+
+Access a Contenticious content tree.
+
+=head1 ATTRIBUTES
+
+=head2 C<pages_dir>
+
+The directory to read from
+
+=head2 C<root_node>
+
+The root of the generated content tree. Will be generated from C<pages_dir>.
+
+=head1 METHODS
+
+=head2 C<find>
+
+    my $node = $content->find('foo/bar');
+
+Find a content node for a given path
+
+=head2 C<for_all_nodes>
+
+    $content->for_all_nodes(sub {
+        my $node = shift;
+        do_something_with($node);
+    });
+
+Execute a subroutine for all content nodes
+
+=head2 C<empty_cache>
+
+Delete cached content
+
+=head1 SEE ALSO
+
+L<Contenticious::Content::Node>, L<Contenticious>
